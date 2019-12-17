452f442b19228f929fbdb4043b39938d541907b9
Mark <markred123@gmail.com>
2019-12-17 22:19:53 +0000

    updates

--- index.php
+++ index.php
@@ -70,14 +70,17 @@ session_start();
 	</section>
 <div class="container">
   <div class="row">
-    <div class="col-sm">
-      One of three columns
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor..</p>
     </div>
-    <div class="col-sm">
-      One of three columns
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor..</p>
     </div>
-    <div class="col-sm">
-      One of three columns
+    <div class="col-sm-4">
+      <h3>Column 3</h3>
+      <p>Lorem ipsum dolor..</p>
     </div>
   </div>
 </div>
