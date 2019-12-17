432395324ff40ae64c12fae385ec6d1312c161f0
Mark <markred123@gmail.com>
2019-12-17 22:17:54 +0000

    updates

--- index.php
+++ index.php
@@ -70,25 +70,14 @@ session_start();
 	</section>
 <div class="container">
   <div class="row">
-    <div class="col">
-      1 of 3
+    <div class="col-sm">
+      One of three columns
     </div>
-    <div class="col-6">
-      2 of 3 (wider)
+    <div class="col-sm">
+      One of three columns
     </div>
-    <div class="col">
-      3 of 3
-    </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      1 of 3
-    </div>
-    <div class="col-5">
-      2 of 3 (wider)
-    </div>
-    <div class="col">
-      3 of 3
+    <div class="col-sm">
+      One of three columns
     </div>
   </div>
 </div>
