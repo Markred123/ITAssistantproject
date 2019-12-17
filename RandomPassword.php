a6e4f2215bc1de9494d57fa1dbcfeef235b252a2
Mark <markred123@gmail.com>
2019-12-17 16:33:11 +0000

    Random password

--- RandomPassword.php
+++ RandomPassword.php
@@ -5,7 +5,7 @@ function generateRandomPassword($length = 16) {
     $charactersLength = strlen($characters);
     $randomString = '';
     for ($i = 0; $i < $length; $i++) {
-        $randomString = $characters[rand(0, $charactersLength - 1)];
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
     }
     return $randomString;
 }
